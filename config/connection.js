0fb15d71e1a130eb83bfebd5d5a56a9de5118388
Antedbell20 <78629236+Antedbell20@users.noreply.github.com>
2023-12-20 22:43:25 -0600

    finished

--- config/connection.js
+++ config/connection.js
@@ -5,9 +5,9 @@ require('dotenv').config();
 // Connect to database
 const connection = mysql.createConnection({
     host : 'localhost',
-    port ; 3306,
+    port : 3306,
     user: 'root',
-    password: process.env.MYSQL_PASSWORD,
+    password: 'password',
     database: 'employees'
 });
 
